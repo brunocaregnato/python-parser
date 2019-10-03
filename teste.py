db9b9a981fb5ac1609ff0b088bdedf7eb2d4f3a0
Bruno Caregnato <33569494+brunocaregnato@users.noreply.github.com>
2019-10-03 13:26:55 -0300

    ajustado teste.py

--- teste.py
+++ teste.py
@@ -1,17 +1,17 @@
 ''' ()() 
-	aaaaa '''  
+	aaaaa '''    
 def main():
     a = 5
     x = 7 #isso eh um comentario
-    if (a < x)
-        a = x
+    '''if (a < x)
+        a = x'''
     else
 	    x = a
     print (a + x)
     exp = (10 * (a + x))
     if ((a+x) > exp | exp == 0)
         a=3
-    elif (a+x <0)
+    elif (a+x < 1)
         x = 7
 	b = 0
     for a in range(100)
